1bfb1451c7457465da499ec42546855a79366ca7
vbalte <54951038+vbalte@users.noreply.github.com>
2019-09-11 11:15:36 -0400

    Update app.js

--- Classwork/jsClassNotesBasic/app.js
+++ Classwork/jsClassNotesBasic/app.js
@@ -20,6 +20,34 @@ let isMarried = false;
 
 console.log(`Hello my name is ${firstName}, I am a ${job}`) //can even use boolean in the sentence 
 
+const now = new Date().getFullYear(); 
+const yearVeda = 2002;
+let fullAge = 18; 
+let isFullAge = now - yearVeda >= fullAge; 
+
+console.log(isFullAge); 
+let ageWill = now - 2002;
+let ageNick = 18;
+let averageAge = (ageNick + ageWill) / 2 ;
+console.log(averageAge); 
+
+let x,y;
+x = y;
+y = 5;
+console.log(x);
+
+
+
+
+
+
+
+
+
+// console.log(now); //returns back the date, pulls from computer 
+
+
+
 
 
 
