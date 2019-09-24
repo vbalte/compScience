60d990d0d68bc757db7536cadc740860f62c61ab
vbalte <54951038+vbalte@users.noreply.github.com>
2019-09-24 11:45:02 -0400

    woot

--- Classwork/jsClassNotesBasic/Creating Avengers/app.js
+++ Classwork/jsClassNotesBasic/Creating Avengers/app.js
@@ -6,7 +6,7 @@ const avengers = [];
 
 
 //create individual avengers ** Objects function constructor
-function Avenger(alias, identity, powers, firstAppearance) {
+/* function Avenger(alias, identity, powers, firstAppearance) {
     this.alias = alias;
     this.identity = identity;
     this.powers = powers;
@@ -14,18 +14,24 @@ function Avenger(alias, identity, powers, firstAppearance) {
     this.yearsActive = function(){
         return new Date().getUTCFullYear() - this.AvengerfirstAppearance;
     }
-}
+} */
 
 /* const ironMan = new Avenger('Iron-Man', 'Tony Stark', 'Smart,Facial Hair,Dead', 1963);
 console.log(ironMan); */
 
 
 //push into array
-function addAvenger(alias, identity, powers, firstAppearance){
+/* function addAvenger(alias, identity, powers, firstAppearance){
     avengers.push(new Avenger(alias, identity, powers, firstAppearance));
-}
+} */
 
-addAvenger('Captain America', 'Steve Rogers', 'Freedom', 1941); 
+//addAvenger('Captain America', 'Steve Rogers', 'Freedom', 1941); 
 
 //display object on screen 
 
+const form = document.querySelector('#hero-form'); n//could also getElementbyId
+const userAlias = document.querySelector('#alias');
+const userPowers = document.querySelector('#powers');
+const userFullName = document.querySelector('#full-name');
+const userFirstAppearance = document.querySelector('#first-appearance');
+const userImage = document.querySelector('#image');
