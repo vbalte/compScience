64037bd7313e05d1abaa6f1752c0ad75ec27a093
vbalte <54951038+vbalte@users.noreply.github.com>
2019-10-08 11:25:52 -0400

    x

--- Classwork/jsClassNotesBasic/app.js
+++ Classwork/jsClassNotesBasic/app.js
@@ -373,3 +373,57 @@ class CD extends Album {
     }
 }
 
+
+
+//challenge 
+
+// class Person {
+//     constructor(age, name){
+//         this.age = age; 
+//         this.name = name; 
+//     }
+
+//  calculateBirthYear() {
+//     return (new Date().getFullYear() - this.age); 
+// }
+
+// }
+
+// const steve = new Person(44, 'Taco');
+// console.log(steve.calculateBirthYear()); 
+
+
+
+// PROJECT 
+
+//one class to create the object person, album, movie ****Model
+//take arguments 
+
+
+
+//***View */ Class for the UI which contains methods 
+//creates/insert the html for the UI DISPLAYS
+
+
+
+//remove something/object 
+
+//clear fields method 
+
+
+
+//controller combines ui and model
+//eventListener handler with function 
+//get values (doc query selector)
+
+//instantiate new class/object album with variable name 
+
+
+//instantiate new UI const ui = new UI()
+
+//call relevant methods
+
+
+
+
+//seperate event handler for removing/deleting 
