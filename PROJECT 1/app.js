ca36b252e6fd19656bbbb1652f647d1adc18c227
Veda <veda.balte@sitechhs.com>
2019-10-30 21:29:16 -0400

    ok

--- PROJECT 1/app.js
+++ PROJECT 1/app.js
@@ -1,59 +1,103 @@
 // PROJECT 
 
-//one class to create the object person, album, movie ****Model
+//one class to create the object person, album, movie
 //take arguments 
 
-const movie = [];
+// const movie = [];
 
 class Movie {
     constructor(title, producer, yearReleased) {
         this.title = title;
         this.producer = producer;
         this.yearReleased = yearReleased;
-    }
+
+
+    this.age = function(){
+        // return new Date().getFullYear() - this.yearReleased; 
+        console.log(new Date().getFullYear() - this.yearReleased); 
 }
+    }}
 
 
 //***View */ Class for the UI which contains methods 
 //creates/insert the html for the UI DISPLAYS
 
-class movieUI {
+class UI {
     //three methods, delete, clear, and push
-     displayMovie(e) {
-          
-        let html = ' <div class="display-movie"><div class="display-title">%album%</div><div class = "display-producer">%producer%</div><div class="display-yearReleased">%yearReleased%</div></div>'
-    
-        let newHtml = html.replace('%title%', movieUI.title);
-        newHtml = newHtml.replace('%producer%', movieUI.producer);
-        newHtml = newHtml.replace('%yearReleased%', movieUI.yearReleased); 
-        document.querySelector(".display").insertAdjacentElement('beforeend', newHtml); 
-    
-        e.preventDefault(); 
-        if(userTitle.value === "") {
-            alert('title missing') 
-        } else if (userProducer.value === "") {
-            alert ('producer missing')      
-            } else if (useryearReleased.value === "") {
-                alert('NO year released')
-            }
-        }
-    }; 
-    
-     removeMovie(e) {
-        if(e.target.parentElement.classList.contains('remove-movie')){
-            e.target.parentElement.parentElement.remove();
-            console.log(e.target.parentElement);
-        }; 
-    
-   function init(){
-        userTitle.value = '';
-        userProducer.value = '';
-        useryearReleased.value = '';
-    } }
-}
-}
+     addMovieToList(movie) {    //Camel case woot wooot 
 
-init(); 
+
+
+        //   const display = document.querySelector('.display');  DONT NEED
+        let html = '<div class="display-movie"><div class="display-title">%title%</div><div class = "display-producer">%producer%</div><div class="display-yearReleased">%yearReleased%</div><div class="display-age"> %age% </div><div class = "remove-movie">X</div>'
+        const currentYear = new Date().getFullYear();
+
+    const age = currentYear - yearReleased.value;
+
+        let newHtml = html.replace('%title%', movie.title);
+        newHtml = newHtml.replace('%producer%', movie.producer);
+        newHtml = newHtml.replace('%yearReleased%', movie.yearReleased); 
+        newHtml = newHtml.replace('%age%', age); 
+        document.querySelector(".display").insertAdjacentHTML('beforeend', newHtml); 
+        console.log(age);
+     }
+     
+        // if(userTitle.value === "") {
+        //     alert('title missing') 
+        // } else if (userProducer.value === "") {
+        //     alert ('producer missing')      
+        //     } else if (useryearReleased.value === "") {
+        //         alert('NO year released')
+        //     }    attempt at if/else statement 
+        
+
+    
+     clearFields () {
+        document.getElementById('title').value = "";
+        document.getElementById('producer').value =""; 
+        document.getElementById('yearReleased').value =""; 
+         
+     } 
+    
+   deleteMovie (target){
+       if (target.className === "remove-movie") {
+           target.parentElement.remove(); 
+       }
+    }
+}
+        
+
+       document.getElementById('movie-form').addEventListener('submit', function(e) {
+        const title = document.getElementById('title').value;
+        const producer = document.getElementById('producer').value;
+        const yearReleased = document.getElementById('yearReleased').value;
+     
+    
+        const movie = new Movie(title, producer, yearReleased);
+
+
+    
+        const ui = new UI();
+        console.log(ui);
+        if(title === '' || producer === '' ||  yearReleased === '' ) {
+            alert ('come on try it again'); 
+        } else {
+        ui.addMovieToList(movie);
+        ui.clearFields();
+        }
+
+        e.preventDefault();
+    });
+    
+    document.querySelector(".display").addEventListener("click", function(e){
+        const ui = new UI();
+        ui.deleteMovie(e.target);
+        ui.clearFields();
+        e.preventDefault();
+    });
+
+
+
 
 
 
@@ -67,25 +111,25 @@ init();
 //controller combines ui and model
 //eventListener handler with function 
 
-function eventListeners(){
-    form.addEventListener('submit', displayMovie);
-    display.addEventListener('click', removeAvenger);
+// function eventListeners(){
+//     form.addEventListener('submit', displayMovie);
+//     display.addEventListener('click', removeAvenger);
     
-}
+// }
 
-const title = document.getElementById('title').value;
-const producer = document.getElementById('producer').value; 
-const yearReleased = document.getElementById('yearReleased').value; 
+// const title = document.getElementById('title').value;
+// const producer = document.getElementById('producer').value; 
+// const yearReleased = document.getElementById('yearReleased').value; 
 
-const movie = new movie(title, producer,yearReleased); 
+// const movie = new movie(title, producer,yearReleased); 
 
-const ui = new UI();
+// const ui = new UI();
 
-ui.displayMovie(album);
+// ui.displayMovie(album);
 
-ui.init();
+// ui.init();
 
-e.preventDefault();
+// e.preventDefault();
 
 
 
@@ -101,6 +145,3 @@ e.preventDefault();
 //call relevant methods
 
 
-
-
-//seperate event handler for removing/deleting 
